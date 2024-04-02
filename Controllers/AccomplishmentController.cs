fa788cc29d74a9344d8a40947455609bc307f8e6
ArkinSolomon <arkin@arkinsolomon.net>
2024-04-02 17:22:51 -0400

    I forgot to add files

---
+++ Controllers/AccomplishmentController.cs
@@ -0,0 +1,141 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAppsProject5.Models;
+
+namespace WebAppsProject5.Controllers;
+
+public class AccomplishmentController(ApplicationContext context) : Controller
+{
+    // GET: Accomplishment
+    public async Task<IActionResult> Index()
+    {
+        return View(await context.Accomplishments.ToListAsync());
+    }
+
+    // GET: Accomplishment/Details/5
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var accomplishment = await context.Accomplishments
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (accomplishment == null)
+        {
+            return NotFound();
+        }
+
+        return View(accomplishment);
+    }
+
+    // GET: Accomplishment/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: Accomplishment/Create
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,Name,Type")] Accomplishment accomplishment)
+    {
+        if (ModelState.IsValid)
+        {
+            context.Add(accomplishment);
+            await context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(accomplishment);
+    }
+
+    // GET: Accomplishment/Edit/5
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var accomplishment = await context.Accomplishments.FindAsync(id);
+        if (accomplishment == null)
+        {
+            return NotFound();
+        }
+        return View(accomplishment);
+    }
+
+    // POST: Accomplishment/Edit/5
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Type")] Accomplishment accomplishment)
+    {
+        if (id != accomplishment.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                context.Update(accomplishment);
+                await context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AccomplishmentExists(accomplishment.Id))
+                {
+                    return NotFound();
+                }
+
+                throw;
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(accomplishment);
+    }
+
+    // GET: Accomplishment/Delete/5
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var accomplishment = await context.Accomplishments
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (accomplishment == null)
+        {
+            return NotFound();
+        }
+
+        return View(accomplishment);
+    }
+
+    // POST: Accomplishment/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var accomplishment = await context.Accomplishments.FindAsync(id);
+        if (accomplishment != null)
+        {
+            context.Accomplishments.Remove(accomplishment);
+        }
+
+        await context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+
+    private bool AccomplishmentExists(int id)
+    {
+        return context.Accomplishments.Any(e => e.Id == id);
+    }
+}
---
+++ Migrations/20240402151236_idk whats going on anymore.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAppsProject5.Migrations
+{
+    /// <inheritdoc />
+    public partial class idkwhatsgoingonanymore : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "Id",
+                table: "Requirements",
+                type: "char(36)",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                collation: "ascii_general_ci");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Courses",
+                type: "Varchar(64)",
+                maxLength: 64,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Requirements",
+                table: "Requirements",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Requirements",
+                table: "Requirements");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Requirements");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Courses",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "Varchar(64)",
+                oldMaxLength: 64)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
